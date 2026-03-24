54f6d2950fa71d51bbb8113b42cdf520e2421eca
Martin <martin@awadlouis.com>
2026-03-24 18:22:47 +0100

    chore: Establish Database Connection

--- src/main/java/com/paylet/paylet/PayletApplication.java
+++ src/main/java/com/paylet/paylet/PayletApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PayletApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PayletApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PayletApplication.class, args);
+    }
 
 }
